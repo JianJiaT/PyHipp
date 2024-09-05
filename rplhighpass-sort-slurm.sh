35e063bcb311cd9376473049508257fe61daec83
AWS ParallelCluster user <ec2-user@ip-10-0-14-193.ap-southeast-1.compute.internal>
2024-09-05 14:20:59 +0000

    Update rplhighpass to make use of separate conda environments

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -16,6 +16,11 @@
 #
 #
 
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -24,3 +29,7 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate
+/data/src/PyHipp/envlist.py $envarg
+
